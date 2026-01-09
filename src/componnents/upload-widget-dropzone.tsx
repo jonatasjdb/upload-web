92345c300bf1875712779ea1ed8001f9b2b19333
Jônatas Dias <mac@192.168.0.16>
2026-01-09 19:34:16 -0300

    Ativando upload para clodflare R2

--- src/componnents/upload-widget-dropzone.tsx
+++ src/componnents/upload-widget-dropzone.tsx
@@ -1,9 +1,12 @@
 import { motion } from "motion/react";
 import { useDropzone } from "react-dropzone";
+import { useUploads } from "../store/uploads";
 import CircularProgressBar from "./ui/circular-progress-bar";
 
 export function UploadWidgetDropzone() {
-	const isThereAnyPendingUpload = true;
+	const { addUploads } = useUploads();
+
+	const isThereAnyPendingUpload = false;
 	const uploadGlobalPercentage = 50;
 
 	const { getRootProps, getInputProps, isDragActive } = useDropzone({
@@ -12,7 +15,9 @@ export function UploadWidgetDropzone() {
 			"image/jpeg": [],
 			"image/png": [],
 		},
-		onDrop(acceptedFiles, fileRejections, event) {},
+		onDrop(acceptedFiles, fileRejections, event) {
+			addUploads(acceptedFiles);
+		},
 	});
 	return (
 		<motion.div
